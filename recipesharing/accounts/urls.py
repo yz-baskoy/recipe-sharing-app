ba685e3883659efc67496e176a31995612fd18a8
yz-baskoy <yziyabaskoy@gmail.com>
2021-03-20 22:09:58 +0300

    gitignore

--- recipesharing/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserRegister
-
-app_name = 'accounts'
-
-urlpatterns = [
-    path('register/', UserRegister.as_view(), name='register'),
-]
--- recipesharing/blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Post, Ingredients, Rate
-
-admin.site.register(Post)
-admin.site.register(Ingredients)
-admin.site.register(Rate)
